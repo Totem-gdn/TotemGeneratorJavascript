06b1077eea01b51c02fcedcf3d02be6bab761b00
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2022-04-24 23:28:50 +0300

    Models updated.

--- Totem-plugin/models/serverDto/Avatar.ts
+++ Totem-plugin/models/serverDto/Avatar.ts
@@ -1,14 +1,45 @@
 interface OwnerPlayer {
-    id: string;
+    id: number;
     name: string;
     public_blockchain_address: string;
 }
 
-interface AvatarDto {
-    id: string;
+interface AvatarListDto {
+    id: number;
     seed: string;
     owner: string;
     skinColor: string;
     hairColor: string;
     _owner_players: OwnerPlayer
+}
+
+// -- Selecting specific avatar
+
+interface GoogleOAuth {
+    id: number;
+    name: string;
+    email: string;
+}
+
+interface Owner {
+    id: number;
+    created_at: number;
+    name: string;
+    email: string;
+    public_blockchain_address: string;
+    google_oauth?: GoogleOAuth;
+}
+
+interface AvatarDto {
+    id: number;
+    created_at: number;
+    seed: string;
+    owner: Owner;
+    sex: SexEnum;
+    skinColor: string;
+    hairColor: string;
+    hairStyle: HairStyleEnum;
+    eyeColor: string;
+    bodyFat: BodyFatEnum;
+    bodyMuscles: BodyMusclesEnum;
 }
